e321b998d1d011d3bf69761b67a55c7dabedc043
Max Jones <14077947+maxrjones@users.noreply.github.com>
2024-07-31 14:18:56 -0400

    Add GitLens VS Code extension

--- install-vscode-ext.sh
+++ install-vscode-ext.sh
@@ -3,7 +3,7 @@
 # Install VSCode extensions.
 # These get installed to $CONDA_PREFIX/envs/notebook/share/code-server/extensions/
 
-extensions=("ms-python.python" "ms-toolsai.jupyter" "ms-python.debugpy" "quarto.quarto")
+extensions=("ms-python.python" "ms-toolsai.jupyter" "ms-python.debugpy" "eamodio.gitlens" "quarto.quarto")
 
 for EXT in "${extensions[@]}"; do
     code-server --install-extension "${EXT}"
